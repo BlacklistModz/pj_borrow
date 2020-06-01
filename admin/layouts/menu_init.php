1399a5c591e94b912e2975be8a870f346e9ae701
BlacklistModz <BlacklistModz.Ton@gmail.com>
2020-06-01 13:19:38 +0700

    UPDATE MENU ADMIN
    - Add Menu borrows for manage

--- admin/layouts/menu_init.php
+++ admin/layouts/menu_init.php
@@ -1,4 +1,5 @@
 <?php
+$menu[] = ["label"=>"จัดการใบสมัครสินเชื่อ", "key"=>"borrows", "url"=>URL."admin/borrows", 'icon'=>'fas fa-file-invoice-dollar'];
 $menu[] = ["label"=>"จัดการข้อมูลลูกค้า", "key"=>"customers", "url"=>URL."admin/customers", 'icon'=>'fa fa-users'];
 $menu[] = ["label"=>"จัดการข้อมูลผู้ใช้งาน", "key"=>"users", "url"=>URL."admin/users", 'icon'=>'fa fa-user'];
 
