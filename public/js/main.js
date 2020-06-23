2078f80fe22148cc9b4ebe83f14df981a2b0211d
BlacklistModz <BlacklistModz.Ton@gmail.com>
2020-06-23 10:59:01 +0700

    FIX BUG Modal
    - Fix bug modal don't alert if syntex code error on dataModal.php

--- public/js/main.js
+++ public/js/main.js
@@ -288,70 +288,76 @@ if ( typeof Object.create !== 'function' ) {
 		},'json');
 	};
 
-	$.fn.setModal = function( res ){
-		var modal = $(".modal");
-
-		if( !res.attr('href') ){
-			$.fn.sweetalert( {type:"error", title:"เกิดข้อผิดพลาด...", text:"กรุณาระบุ a.href สำหรับโหลดข้อมูล", "timer":2000} );
-			return false;
-		}
-
-		$.get( res.attr('href'), function( result ) {
-
-			if( !result.bgClose ){
-				modal.modal({backdrop: 'static', keyboard: false});
+	$.fn.loadModal = function( res ){
+		$.ajax({
+			url: res.attr('href'),
+			dataType : 'json',
+			success : function( result ) {
+				$.fn.setModal( result );
+			},
+			error : function() {
+				$.fn.sweetalert( {type:"error", title:"เกิดข้อผิดพลาด...", "timer":2000} );
 			}
-
-			var setCenter = result.center || "true";
-
-			var $elem = $(result.form || '<div>').addClass("modal-content").addClass( result.addClass ).addClass( result.style ? 'style-'+result.style: '' );
-
-			if( result.hiddenInput ){
-				$.each( result.hiddenInput, function(i, input) {
-					var hiddenInput = $.fn.setHiddenInput( input );
-					$elem.append( hiddenInput );
-				});
-			}
-
-			if( result.title || result.headClose ){
-				$elem.append( $('<div>', {class:"modal-header"}) );
-			}
-			if( result.title ){
-				$elem.find('.modal-header').append(
-					$('<h5>', {class:'modal-title'}).html( result.title )
-				);
-			}
-			if( result.headClose ){
-				$elem.find('.modal-header').append( 
-					$('<button>', {class:'close', 'data-dismiss':'modal', 'aria-label':'Exit'}).append( 
-						$('<span>', {'aria-hidden':'true'}).html('&times;') 
-					)
-				);
-			}
-			if( result.body ){
-				$elem.append( $('<div>', {class:"modal-body"}).html( result.body ) );
-			}
-			if( result.btnclose || result.btnsubmit ){
-				$elem.append( $('<div>', {class:"modal-footer clearfix"}) );
-				if( result.btnclose ){
-					$elem.find('.modal-footer').append( result.btnclose );
-				}
-				if( result.btnsubmit ){
-					$elem.find('.modal-footer').append( result.btnsubmit );
-				}
-			}
-			if( setCenter == "true" ){
-				modal.find('.modal-dialog').addClass('modal-dialog-centered')
-			}
-
-			modal.find('.modal-dialog').empty(); //Clear Old Modal
-			modal.find('.modal-dialog').addClass( result.dialogClass ).append( $elem );
-			modal.modal('show');
-		}, 'json');
+		});
 	};
 
 	$.fn.setHiddenInput = function( input ){
 		return $('<input>', {type:"hidden", "name":input.name, "value":input.value});
+	};
+
+	$.fn.setModal = function( result ){
+
+		var modal = $(".modal");
+
+		if( !result.bgClose ){
+			modal.modal({backdrop: 'static', keyboard: false});
+		}
+
+		var setCenter = result.center || "true";
+
+		var $elem = $(result.form || '<div>').addClass("modal-content").addClass( result.addClass ).addClass( result.style ? 'style-'+result.style: '' );
+
+		if( result.hiddenInput ){
+			$.each( result.hiddenInput, function(i, input) {
+				var hiddenInput = $.fn.setHiddenInput( input );
+				$elem.append( hiddenInput );
+			});
+		}
+
+		if( result.title || result.headClose ){
+			$elem.append( $('<div>', {class:"modal-header"}) );
+		}
+		if( result.title ){
+			$elem.find('.modal-header').append(
+				$('<h5>', {class:'modal-title'}).html( result.title )
+				);
+		}
+		if( result.headClose ){
+			$elem.find('.modal-header').append( 
+				$('<button>', {class:'close', 'data-dismiss':'modal', 'aria-label':'Exit'}).append( 
+					$('<span>', {'aria-hidden':'true'}).html('&times;') 
+					)
+				);
+		}
+		if( result.body ){
+			$elem.append( $('<div>', {class:"modal-body"}).html( result.body ) );
+		}
+		if( result.btnclose || result.btnsubmit ){
+			$elem.append( $('<div>', {class:"modal-footer clearfix"}) );
+			if( result.btnclose ){
+				$elem.find('.modal-footer').append( result.btnclose );
+			}
+			if( result.btnsubmit ){
+				$elem.find('.modal-footer').append( result.btnsubmit );
+			}
+		}
+		if( setCenter == "true" ){
+			modal.find('.modal-dialog').addClass('modal-dialog-centered')
+		}
+
+		modal.find('.modal-dialog').empty(); //Clear Old Modal
+		modal.find('.modal-dialog').addClass( result.dialogClass ).append( $elem );
+		modal.modal('show');
 	};
 
 	$.fn.clearModal = function(){
@@ -420,7 +426,7 @@ $(".js-doc-amphur").change(function(){
 });
 
 $("a[data-plugins=modal]").click(function(){
-	$.fn.setModal( $(this) );
+	$.fn.loadModal( $(this) );
 	return false;
 });
 
